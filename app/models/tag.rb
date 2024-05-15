1fe51e4ec7d6800e30b0f25725bf6b4d24ec696b
Emina <emina@simplify.ba>
2024-05-15 18:19:09 +0200

    Add tags to posts controller

---
+++ app/models/tag.rb
@@ -0,0 +1,18 @@
+class Tag < ApplicationRecord
+  has_and_belongs_to_many :posts
+  before_validation :create_slug, on: :create
+
+  private
+
+  def create_slug
+    return if self.name.nil?
+    
+    slug = self.name.parameterize
+    if Tag.find_by(slug: slug)
+      self.slug = slug + "-" + SecureRandom.hex(6)
+    else
+      self.slug = slug
+    end
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_10_115937) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_13_211129) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -63,6 +63,20 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_10_115937) do
     t.datetime "published_at"
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "posts_tags", id: false, force: :cascade do |t|
+    t.bigint "post_id"
+    t.bigint "tag_id"
+    t.index ["post_id"], name: "index_posts_tags_on_post_id"
+    t.index ["tag_id"], name: "index_posts_tags_on_tag_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "slug", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
