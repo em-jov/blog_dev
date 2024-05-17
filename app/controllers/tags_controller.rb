876c2ecba1d0c20646a668eb0a639844676a3cad
Emina <emina@simplify.ba>
2024-05-17 16:27:55 +0200

    Add tags controller

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,5 +1,5 @@
 class TagsController < ApplicationController
-  before_action :find_tag, only: %i[show]
+  before_action :find_tag, only: %i[show edit update destroy]
 
   def index
     @tags = Tag.all
@@ -7,10 +7,24 @@ class TagsController < ApplicationController
 
   def show; end
 
+  def edit; end
+
+  def update
+    redirect_to({ action: :show, slug: @tag.slug }, notice: 'Tag updated.') and return if @tag.update(tags_params)
+
+    render :edit, status: :unprocessable_entity
+  end
+
+  def destroy
+    @tag.destroy
+    flash[:notice] = 'Tag deleted'
+    redirect_to tags_path
+  end
+
   private
 
   def find_tag
-    @tag = Tag.find_by(slug: params[:slug])
+    @tag = Tag.find_by(slug: params[:slug]) #|| Tag.find(params[:id])
   end
 
   def tags_params
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,6 +2,8 @@ class Tag < ApplicationRecord
   has_and_belongs_to_many :posts
   before_validation :create_slug, on: :create
 
+  validates :name, :slug, presence: true, uniqueness: true
+
   private
 
   def create_slug
